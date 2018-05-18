5c81e72f8bcd047f1f2afa2fdf029c74771f7ffe
BobbyYan <33873055+BobbyYan@users.noreply.github.com>
2018-05-18 21:20:37 +0300

    Add files via upload

---
+++ logingin/src/logingin/loginUserPas.java
@@ -0,0 +1,22 @@
+package logingin;
+
+import java.util.Scanner;
+
+public class loginUserPas {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your username:");
+		String username = input.nextLine();
+		System.out.println("Enter your password:");
+		String password = input.nextLine();	
+		/* 
+		 * if Authenticator(true)
+		 * System.out.println("Login Succes");
+		 * System.out.println("Hello " + username);
+		 * else 
+		 * System.out.println(Wrong username or password, please try again);
+		 * 
+		 */
+}
+}
